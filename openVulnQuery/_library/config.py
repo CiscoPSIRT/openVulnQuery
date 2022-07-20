b26158ea3310bd7a6d043a7e497832c74a7881f0
Omar Santos <os@cisco.com>
2022-07-19 20:26:55 -0400

    Adding openVulnAPI v2 URI

--- openVulnQuery/_library/config.py
+++ openVulnQuery/_library/config.py
@@ -10,4 +10,4 @@ CLIENT_ID = ""
 CLIENT_SECRET = ""
 
 REQUEST_TOKEN_URL = "https://cloudsso.cisco.com/as/token.oauth2"
-API_URL = "https://api.cisco.com/security/advisories"
+API_URL = "https://api.cisco.com/security/advisories/v2"
